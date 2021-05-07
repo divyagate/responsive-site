17257b0e871a96d2b8bb4c73f5fa0ca3c89901e6
divya <gatedivya123@gmail.com>
2021-05-07 00:08:01 -0400

    responsive site

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container-fluid max-container-size">
+          <div className="row">
+            <div className="col-sm-12 col-md-6 col-lg-4">
+                <div className="left-section">
+                  <div className="description">
+                    <p className="large-text">Top 10 Hottest Flavours</p>
+                    <p className="large-text">Summer 2021</p>
+                  </div>
+                </div>
+            </div>
+            <div className="col-sm-12 col-md-6 col-lg-8">
+                <div className="right-section">
+                    <div className="row">
+                        <div className="col-sm-12 col-md-12 col-lg-6">
+                        <div className="imageTop-left">
+                        <div className="section-button">
+                          Get it
+                        </div>
+                        </div>
+                        </div>
+                        <div className="col-sm-12 col-md-12 col-lg-6">
+                        <div className="imageTop-right">
+                        <div className="section-button">
+                          Get it
+                        </div>
+                        </div>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col">
+                        <div className="image-bottom">
+                        <div className="bottom-description">
+                          <p className="large-text">Our Ice Cream Philosophy</p>
+                          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                          <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                        </div>
+                        </div>
+                        </div>
+                      </div>
+                </div>
+            </div>
+          </div>
+      </div>
     </div>
   );
 }
